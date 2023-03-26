be0a98b03ad6c31266f5333d09803ba64893cd45
Danila Kornev <danilakornev92@gmail.com>
2023-03-26 23:16:47 +0300

    final

--- Homework3/ViewController.swift
+++ Homework3/ViewController.swift
@@ -10,44 +10,67 @@ import UIKit
 final class ViewController: UIViewController {
 
     
-    @IBOutlet var palette: UIView!
+    @IBOutlet private var palette: UIView!
     
-    @IBOutlet var redColorSlider: UISlider!
-    @IBOutlet var greenColorSlider: UISlider!
-    @IBOutlet var blueColorSlider: UISlider!
+    @IBOutlet private var redColorSlider: UISlider!
+    @IBOutlet private var greenColorSlider: UISlider!
+    @IBOutlet private var blueColorSlider: UISlider!
     
-    @IBOutlet var redColorValueLabel: UILabel!
-    @IBOutlet var greenColorValueLabel: UILabel!
-    @IBOutlet var blueColorValueLabel: UILabel!
+    @IBOutlet private var redColorValueLabel: UILabel!
+    @IBOutlet private var greenColorValueLabel: UILabel!
+    @IBOutlet private var blueColorValueLabel: UILabel!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        palette.backgroundColor = UIColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)
+        redColorValueLabel.text = redColorSlider.value.formatted(
+            .number.precision(.fractionLength(2))
+        )
+        greenColorValueLabel.text = greenColorSlider.value.formatted(
+            .number.precision(.fractionLength(2))
+        )
+        blueColorValueLabel.text = blueColorSlider.value.formatted(
+            .number.precision(.fractionLength(2))
+        )
         
+        setPaletteColor()
+        
+        palette.layer.cornerRadius = 10
         
     }
 
-    @IBAction func redColorSliderAction() {
-        redColorValueLabel.text = redColorSlider.value.formatted(.number.precision(.fractionLength(2)))
+    @IBAction private func redColorSliderAction() {
+        redColorValueLabel.text = redColorSlider.value.formatted(
+            .number.precision(.fractionLength(2))
+        )
+        
         setPaletteColor()
         
         
     }
     
-    @IBAction func greenColorSliderAction() {
-        greenColorValueLabel.text = greenColorSlider.value.formatted(.number.precision(.fractionLength(2)))
+    @IBAction private func greenColorSliderAction() {
+        greenColorValueLabel.text = greenColorSlider.value.formatted(
+            .number.precision(.fractionLength(2))
+        )
         
         setPaletteColor()
     }
     
-    @IBAction func blueColorSliderAction() {
-        blueColorValueLabel.text = blueColorSlider.value.formatted(.number.precision(.fractionLength(2)))
+    @IBAction private func blueColorSliderAction() {
+        blueColorValueLabel.text = blueColorSlider.value.formatted(
+            .number.precision(.fractionLength(2))
+        )
         
         setPaletteColor()
     }
+    
     private func setPaletteColor () {
-        palette.backgroundColor = UIColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)
+        palette.backgroundColor = UIColor(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(blueColorSlider.value),
+            alpha: 1)
     }
 }
 
