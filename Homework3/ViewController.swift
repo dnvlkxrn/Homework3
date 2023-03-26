39d8870812ae9121288316c6741765bda76e8ecf
Danila Kornev <danilakornev92@gmail.com>
2023-03-26 22:46:31 +0300

    logic realization

--- Homework3/ViewController.swift
+++ Homework3/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    
+    @IBOutlet var palette: UIView!
+    
+    @IBOutlet var redColorSlider: UISlider!
+    @IBOutlet var greenColorSlider: UISlider!
+    @IBOutlet var blueColorSlider: UISlider!
+    
+    @IBOutlet var redColorValueLabel: UILabel!
+    @IBOutlet var greenColorValueLabel: UILabel!
+    @IBOutlet var blueColorValueLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        palette.backgroundColor = UIColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)
+        
+        
     }
 
-
+    @IBAction func redColorSliderAction() {
+        redColorValueLabel.text = redColorSlider.value.formatted(.number.precision(.fractionLength(2)))
+        setPaletteColor()
+        
+        
+    }
+    
+    @IBAction func greenColorSliderAction() {
+        greenColorValueLabel.text = greenColorSlider.value.formatted(.number.precision(.fractionLength(2)))
+        
+        setPaletteColor()
+    }
+    
+    @IBAction func blueColorSliderAction() {
+        blueColorValueLabel.text = blueColorSlider.value.formatted(.number.precision(.fractionLength(2)))
+        
+        setPaletteColor()
+    }
+    private func setPaletteColor () {
+        palette.backgroundColor = UIColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)
+    }
 }
 
